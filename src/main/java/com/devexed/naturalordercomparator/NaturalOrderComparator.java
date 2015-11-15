36907d64d49ffe6966b549a6db587c9b0e0e4e59
Johan Bergström <johan.carl.bergstrom@gmail.com>
2015-11-15 23:34:11 +0100

    Clean up and include number chunk bytes in lookup key generation

--- src/main/java/com/devexed/naturalordercomparator/NaturalOrderComparator.java
+++ src/main/java/com/devexed/naturalordercomparator/NaturalOrderComparator.java
@@ -1,10 +1,10 @@
 package com.devexed.naturalordercomparator;
 
+import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
 import java.text.Collator;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.Normalizer;
 import java.text.ParseException;
 import java.util.Comparator;
 import java.util.Locale;
@@ -13,11 +13,10 @@ import java.util.regex.Pattern;
 
 
 /**
- * <p>Comparator for ordering strings in
- * <a href="https://en.wikipedia.org/wiki/Natural_sort_order">natural order</a>. Compares strings
- * case-insensitively and compares any numbers found in the string according to their magnitude,
- * rather than digit by digit. Additionally merges whitespace in text and ignores any whitespace
- * around numbers for even more human friendliness.</p>
+ * <p>Comparator for ordering strings in <a href="https://en.wikipedia.org/wiki/Natural_sort_order">natural order</a>.
+ * Compares strings case-insensitively and compares any numbers found in the string according to their magnitude, rather
+ * than digit by digit. Additionally merges whitespace in text and ignores any whitespace around numbers for even more
+ * human friendliness.</p>
  */
 public final class NaturalOrderComparator implements Comparator<String> {
 
@@ -29,10 +28,28 @@ public final class NaturalOrderComparator implements Comparator<String> {
         return textCollator;
     }
 
-    private static final Pattern whitespaceTrimmer = Pattern.compile("\\s+");
+    private static byte[] normalizeBigDecimal(BigDecimal number) {
+        long doubleBits = Double.doubleToLongBits(number.doubleValue());
+
+        return new byte[] {
+                (byte) (doubleBits >> 56),
+                (byte) (doubleBits >> 48),
+                (byte) (doubleBits >> 40),
+                (byte) (doubleBits >> 32),
+                (byte) (doubleBits >> 24),
+                (byte) (doubleBits >> 16),
+                (byte) (doubleBits >> 8),
+                (byte) doubleBits
+        };
+    }
+
+    // Unicode whitespace and digit matching.
+    private static final String digitPatternString = "\\p{Nd}";
+    private static final String whitespacePatternString = "[\\u0009-\\u000D\\u0020\\u0085\\u00A0\\u1680\\u180E\\u2000-\\u200A\\u2028\\u2029\\u202F\\u205F\\u3000]+";
+    private static final Pattern whitespacePattern = Pattern.compile(whitespacePatternString);
+    private static final char whitespaceReplacement = ' ';
 
     private final Collator textCollator;
-    private final Pattern decimalPatten;
     private final Pattern decimalChunkPatten;
     private final DecimalFormat decimalFormat;
 
@@ -41,78 +58,109 @@ public final class NaturalOrderComparator implements Comparator<String> {
     }
 
     public NaturalOrderComparator(Locale locale) {
-        this(locale, createDefaultCollator(locale));
+        this(createDefaultCollator(locale), new DecimalFormatSymbols(locale));
     }
 
-    public NaturalOrderComparator(Locale locale, Collator textCollator) {
+    public NaturalOrderComparator(Collator textCollator, DecimalFormatSymbols symbols) {
         this.textCollator = textCollator;
 
         // Relevant number symbols coerced to non-null strings.
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
+        String minusSign = "" + symbols.getMinusSign();
         String groupingSeparator = "" + symbols.getGroupingSeparator();
         String decimalSeparator = "" + symbols.getDecimalSeparator();
-        String minus = "" + symbols.getMinusSign();
 
-        // Matches digits in unicode.
-        String digit = "\\p{Nd}";
-
-        // Pattern to match the numbers.
+        // Pattern to match numbers in the string.
         StringBuilder patternBuilder = new StringBuilder();
-        patternBuilder.append(Pattern.quote(minus)).append("?");
+        patternBuilder.append("(.*?)").append(whitespaceReplacement).append("*(");
 
-        if (!groupingSeparator.isEmpty()) {
-            // Create pattern that includes grouped numbers e.g. 1,000,000,000
-            patternBuilder.append("(").append(digit).append("+")
-                    .append(Pattern.quote(groupingSeparator))
-                    .append(")*");
-        }
+        // Include negative numbers...
+        if (!minusSign.isEmpty()) patternBuilder.append(Pattern.quote(minusSign)).append("?");
 
-        patternBuilder.append(digit).append("+");
+        // Include grouped number part (e.g. 1,000,000,000)...
+        if (!groupingSeparator.isEmpty())
+            patternBuilder.append("(").append(digitPatternString).append("+")
+                    .append(Pattern.quote(groupingSeparator)).append(")*");
 
-        // Include decimal part if the separator exists.
+        // Include integer number part...
+        patternBuilder.append(digitPatternString).append("+");
+
+        // Include decimal part...
         if (!decimalSeparator.isEmpty())
-            patternBuilder.append("(").append(Pattern.quote(decimalSeparator)).append(digit).append("+)?");
+            patternBuilder.append("(").append(Pattern.quote(decimalSeparator)).append(digitPatternString).append("+)?");
 
-        String decimalPatternString = patternBuilder.toString();
-        decimalPatten = Pattern.compile(decimalPatternString);
-        decimalChunkPatten = Pattern.compile("(.*?)\\s*(" + decimalPatternString + ")");
+        patternBuilder.append(")");
+        decimalChunkPatten = Pattern.compile(patternBuilder.toString());
 
-        // Format to parse the numbers.
-        decimalFormat = new DecimalFormat("#,#00.#", symbols);
+        // Format to parse the numbers with three digits per group and a decimal part.
+        decimalFormat = new DecimalFormat("#,##0.#", symbols);
         decimalFormat.setParseBigDecimal(true);
     }
 
+    private String trimText(String text) {
+        // Collapse whitespace.
+        text = whitespacePattern.matcher(text).replaceAll("" + whitespaceReplacement);
+
+        // Trim start whitespace.
+        if (!text.isEmpty() && text.charAt(0) == whitespaceReplacement) text = text.substring(1);
+
+        // Trim end whitespace.
+        if (!text.isEmpty() && text.charAt(text.length() - 1) == whitespaceReplacement)
+            text = text.substring(0, text.length() - 1);
+
+        return text;
+    }
+
+    private int compareText(String lhs, String rhs) {
+        return textCollator.compare(trimText(lhs), trimText(rhs));
+    }
+
     /**
-     * <p>Normalize a string according to this comparators rules. Will create key from the string that can be used for
+     * <p>Normalize a string according to this comparators rules. Will create a key from the string that can be used for
      * first pass lookups, restricting the set of values that need to be compared for equality by the {@link #compare}
      * method.</p>
-     * <p>If <code>compare(a, b) == 0</code> then <code>normalizeForLookup(a).equals(normalizeForLookup(b)) ==
-     * true</code></p>
+     * <p>If <code>compare(a, b)</code> is <code>0</code> then <code>normalizeForLookup(a)</code> equals
+     * <code>normalizeForLookup(b)</code>.
+     * <p>Note that unlike {@link Collator#getCollationKey(String)} <strong>the key generated by this method can not be
+     * used for ordering of strings</strong>, only for comparing their equality.</p>
      *
      * @param text The text to normalize.
      * @return The key in byte array form.
      */
     public byte[] normalizeForLookup(String text) {
-        // Remove numbers.
-        text = decimalPatten.matcher(text).replaceAll("");
+        // Normalize tet and number parts separately and append to byte array.
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        Matcher m = decimalChunkPatten.matcher(text);
+        int e = 0;
 
-        // Strip whitespace.
-        text = whitespaceTrimmer.matcher(text).replaceAll(" ").trim();
+        while (m.find()) {
+            String t = m.group(1);
+            String td = m.group(2);
 
-        // Return the bytes of the collation key.
-        return textCollator.getCollationKey(text).toByteArray();
-    }
+            byte[] tb = textCollator.getCollationKey(t).toByteArray();
+            bytes.write(tb, 0, tb.length);
 
-    private int compareText(String lhs, String rhs) {
-        return textCollator.compare(lhs, rhs);
+            try {
+                // Parse numbers as big decimal and compare.
+                BigDecimal d = (BigDecimal) decimalFormat.parse(td);
+                byte[] db = normalizeBigDecimal(d);
+                bytes.write(db, 0, db.length);
+            } catch (ParseException ex) {
+                // Ignorable barring some bug in the Java implementation.
+                throw new RuntimeException(ex);
+            }
+
+            e = m.end();
+        }
+
+        // Add final text segment.
+        byte[] tb = textCollator.getCollationKey(text.substring(e)).toByteArray();
+        bytes.write(tb, 0, tb.length);
+
+        return bytes.toByteArray();
     }
 
     @Override
     public int compare(String lhs, String rhs) {
-        // Trim whitespace.
-        lhs = whitespaceTrimmer.matcher(lhs).replaceAll(" ").trim();
-        rhs = whitespaceTrimmer.matcher(rhs).replaceAll(" ").trim();
-
         // Match strings against number pattern and compare the segments in each string one by one.
         Matcher lm = decimalChunkPatten.matcher(lhs);
         Matcher rm = decimalChunkPatten.matcher(rhs);
@@ -138,18 +186,16 @@ public final class NaturalOrderComparator implements Comparator<String> {
 
                 int numberCompare = ld.compareTo(rd);
                 if (numberCompare != 0) return numberCompare;
-            } catch (ParseException e) {
-                // Should be ignorable, but in case of a parse error (possibly due to a weird
-                // locale) defer to comparing as strings.
-                int numberAsTextCompare = compareText(ltd, rtd);
-                if (numberAsTextCompare != 0) return numberAsTextCompare;
+            } catch (ParseException ex) {
+                // Ignorable barring some bug in the Java implementation.
+                throw new RuntimeException(ex);
             }
 
             le = lm.end();
             re = rm.end();
         }
 
-        // Compare final non-number segment.
+        // Compare final text segment.
         return compareText(lhs.substring(le), rhs.substring(re));
     }
 
--- src/test/java/com/devexed/naturalordercomparator/NaturalOrderComparatorTest.java
+++ src/test/java/com/devexed/naturalordercomparator/NaturalOrderComparatorTest.java
@@ -1,20 +1,15 @@
 package com.devexed.naturalordercomparator;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 import java.text.Collator;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.ParseException;
-import java.util.Arrays;
 import java.util.Locale;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.number.OrderingComparison.comparesEqualTo;
-import static org.hamcrest.number.OrderingComparison.greaterThan;
-import static org.hamcrest.number.OrderingComparison.lessThan;
 
 public class NaturalOrderComparatorTest extends TestCase {
 
@@ -33,43 +28,55 @@ public class NaturalOrderComparatorTest extends TestCase {
         return format.format(number);
     }
 
-    private static void compareNumbers(Locale locale, double a, double b) {
-        NaturalOrderComparator comp = new NaturalOrderComparator(locale);
+    private static double randomNumber(double from, double to) {
+        return from + Math.random() * (to - from);
+    }
+
+    private static int sign(int i) {
+        return i == 0 ? 0 : (i < 0) ? -1 : 1;
+    }
+
+    private static void compareNumbers(Locale locale, Collator collator, double a, double b) {
+        String as = "ab   c " + formatDouble(locale, a) + ";;234";
+        String bs = "ab  c " +  formatDouble(locale, b) + ";;234   ";
+
+        NaturalOrderComparator comp = new NaturalOrderComparator(collator, new DecimalFormatSymbols(locale));
         int expectedResult = Double.compare(a, b);
-        int compareResult = comp.compare(
-                "abc " + formatDouble(locale, a),
-                "abc " + formatDouble(locale, b));
-
-        if (expectedResult == 0) assertThat(compareResult, comparesEqualTo(0));
-        else if (expectedResult < 0) assertThat(compareResult, lessThan(0));
-        else if (expectedResult > 0) assertThat(compareResult, greaterThan(0));
+        int compareResult = comp.compare(as, bs);
+        assertThat(sign(compareResult), is(sign(expectedResult)));
     }
 
-    public void testSortsDecimalNumbers() {
+    public void testSortNumbers() {
+        // Test numbers with and without minus sign, groups, and decimals.
         for (Locale locale: Locale.getAvailableLocales()) {
-            double a = Math.random();
-            double b = Math.random();
-            compareNumbers(locale, a, b);
-        }
-    }
+            for (int collationStrength : new int[]{ Collator.PRIMARY, Collator.SECONDARY, Collator.TERTIARY }) {
+                Collator collator = Collator.getInstance();
+                collator.setStrength(collationStrength);
 
-    public void testSortsGroupedNumbers() {
-        for (Locale locale: Locale.getAvailableLocales()) {
-            double a = Math.random() * 1000000;
-            double b = Math.random() * 1000000000;
-            compareNumbers(locale, a, b);
+                double million = 1000000;
+                double a = randomNumber(-million, million);
+                double b = randomNumber(-million, million);
+
+                compareNumbers(locale, collator, a, b);
+            }
         }
     }
 
     public void testComparesNormalizedKeys() {
-        // Only test locales with basic collation for now.
-        for (Locale locale: new Locale[] { Locale.ENGLISH, Locale.GERMAN, Locale.FRANCE, Locale.CHINESE, Locale.JAPAN }) {
-            double a = Math.random() * 1000000;
-            double b = Math.random() * 1000000000;
-
+        // Only test locales where changing case matches collation comparison.
+        for (Locale locale: new Locale[] {
+                Locale.ENGLISH,
+                Locale.GERMAN,
+                Locale.FRANCE,
+                Locale.CHINESE,
+                Locale.JAPAN
+        }) {
+            double million = 1000000;
+            String n = formatDouble(locale, randomNumber(-million, million));
             NaturalOrderComparator comp = new NaturalOrderComparator(locale);
-            byte[] keyA = comp.normalizeForLookup("abc ".toUpperCase(locale) + formatDouble(locale, a) + " hsd");
-            byte[] keyB = comp.normalizeForLookup("abc ".toLowerCase(locale) + formatDouble(locale, b) + " hsd");
+
+            byte[] keyA = comp.normalizeForLookup("abc ".toUpperCase(locale) + n + " hsd".toLowerCase(locale));
+            byte[] keyB = comp.normalizeForLookup("abc ".toLowerCase(locale) + n + " hsd".toUpperCase(locale));
 
             assertThat(keyA, is(keyB));
         }
